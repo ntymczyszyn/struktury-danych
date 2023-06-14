c587d90afe4867fe1fc67eaef7560a8844985b79
Natalia <natalia.ewa.tymczyszyn@gmail.com>
2023-06-14 19:35:24 +0200

    tests for AVL

--- tests/performance_tests.cpp
+++ tests/performance_tests.cpp
@@ -1,31 +1,55 @@
-
 #include "../include/SkipList.h"
 #include "../include/AVLtree.h"
 #include "../src/SkipList.cpp"
 #include "../src/AVLtree.cpp"
 #include <memory>
 #include <chrono>
+#include <fstream>
 
-void AVL_test(int n);
+void AVL_test(int number, std::ofstream& results, std::ifstream& data);
+void skipList_test(int number, std::ofstream& results, std::ifstream& data);
 
 int main(){
-    // do dokończenia
-    std::vector<Movie> movies;
-    std::fstream testFile ("../wyniki4.csv", std::ios::app);
+    std::ofstream resultsOrdered ("../resultsPNOrdered.csv", std::ios::app);
+    std::ofstream resultsReverseOrder ("../resultsPNReverseOrder.csv", std::ios::app);
+    std::ofstream resultsRandomOrder ("../resultsPNRandomOrder.csv", std::ios::app);
 
-    std::vector<int> n {10, 100, 10000, 20000, 50000, 100000, 200000, 300000,
-                       400000, 500000, 600000, 700000, 800000, 900000, 1000000};
-    // inserting
-    // deleting
+    std::ifstream orderedData ("../tests/PN_ordered_numbers.csv");
+    std::ifstream reverseOrderedData ("../tests/PN_reversed_numbers.csv");
+    std::ifstream randomOrderedData ("../tests/PN_random_numbers.csv");
 
-    for (const auto& number : n){
-        readData(movies, number);
-        auto duration_start = std::chrono::high_resolution_clock::now();
-        heapSort(movies);
-        auto duration_end = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration<double, std::milli>(duration_end - duration_start).count();
-        testFile << "heapsort, " << number << ", " << duration << "\n";
+    // można podokładać jakbyśmy chciały więcej/ mnie zależy jakie to wydajne wyjdzie
+    std::vector<int> n {10, 100, 10000, 20000, 50000, 100000, 200000, 300000};
+                       //400000, 500000, 600000, 700000, 800000, 900000, 1000000};
+    std::vector<int> test {10, 20};
+    for (const auto& number : test){
+        AVL_test(number, resultsOrdered, orderedData);
+        AVL_test(number, resultsReverseOrder, orderedData);
+        AVL_test(number, resultsRandomOrder, orderedData);
     }
 
     return 0;
+}
+
+void AVL_test(int number, std::ofstream& results, std::ifstream& data){
+    // on się sam ogarnie po wyjściu ze scopu
+    std::unique_ptr<AVLtree<int>> Tree = std::make_unique<AVLtree<int>>();
+    int stream{};
+    auto duration_start = std::chrono::high_resolution_clock::now();
+    for (int i{0}; i < number; ++i) {
+        data >> stream;
+        Tree->insert(stream);
+    }
+    auto duration_end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration<double, std::milli>(duration_end - duration_start).count();
+    results << "AVL, " << "insertion, " << number << ", " << duration << "\n";
+// jaka kolejność usuwania??
+    duration_start = std::chrono::high_resolution_clock::now();
+    for (int i{0}; i < number; ++i) {
+        data >> stream;
+        Tree->insert(stream);
+    }
+    duration_end = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration<double, std::milli>(duration_end - duration_start).count();
+    results << "AVL, " << "deletion, " << number << ", " << duration << "\n";
 }
