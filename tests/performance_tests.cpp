139790c441e2f8c5b9e32ada7260eaca61b905d2
Natalia Tymczyszyn <natalia.ewa.tymczyszyn@gmail.com>
2023-06-15 09:52:40 +0200

    fixed performance_tests.cpp

--- tests/performance_tests.cpp
+++ tests/performance_tests.cpp
@@ -6,48 +6,38 @@
 #include <chrono>
 #include <fstream>
 
-void AVL_test(int number, std::ofstream& resultsIn, std::ofstream& resultsDel, std::ifstream& data);
-void skipList_test(int number, std::ofstream& resultsIn, std::ofstream& resultsDel, std::ifstream& data);
+void AVL_test(int number, int type, std::ifstream& data);
+void skipList_test(int number, int type, std::ifstream& data);
 
 int main(){
     std::ifstream orderedData ("../tests/PN_ordered_numbers.csv");
     std::ifstream reverseOrderedData ("../tests/PN_reversed_numbers.csv");
     std::ifstream randomOrderedData ("../tests/PN_random_numbers.csv");
 
-    std::ofstream resInOrderedAVL ("../tests/results/AVL/resultsPNOrderedIN.csv", std::ios::app);
-    std::ofstream resInReverseOrderAVL("../tests/results/AVL/resultsPNReverseOrderIN.csv", std::ios::app);
-    std::ofstream resInRandomOrderAVL ("../tests/results/AVL/resultsPNRandomOrderIN.csv", std::ios::app);
-
-    std::ofstream resDelOrderedAVL ("../tests/results/AVL/resultsPNOrderedDEL.csv", std::ios::app);
-    std::ofstream resDelReverseOrderAVL("../tests/results/AVL/resultsPNReverseOrderDEL.csv", std::ios::app);
-    std::ofstream resDelRandomOrderAVL ("../tests/results/AVL/resultsPNRandomOrderDEL.csv", std::ios::app);
-
-    std::ofstream resInOrderedSL ("../tests/results/SL/resultsPNOrderedIN.csv", std::ios::app);
-    std::ofstream resInReverseOrderSL("../tests/results/SL/resultsPNReverseOrderIN.csv", std::ios::app);
-    std::ofstream resInRandomOrderSL ("../tests/results/SL/resultsPNRandomOrderIN.csv", std::ios::app);
-
-    std::ofstream resDelOrderedSL ("../tests/results/SL/resultsPNOrderedDEL.csv", std::ios::app);
-    std::ofstream resDelReverseOrderSL("../tests/results/SL/resultsPNReverseOrderDEL.csv", std::ios::app);
-    std::ofstream resDelRandomOrderSL ("../tests/results/SL/resultsPNRandomOrderDEL.csv", std::ios::app);
-
-
     std::vector<int> n {10, 100, 10000, 20000, 50000, 100000, 200000, 300000};
                        //400000, 500000, 600000, 700000, 800000, 900000, 1000000};
     std::vector<int> test {10, 20};
 
     for (const auto& number : test){
-//        AVL_test(number, resInOrderedAVL, resDelOrderedAVL, orderedData);
-//        AVL_test(number, resInReverseOrderAVL,resDelReverseOrderAVL, orderedData);
-//        AVL_test(number, resInRandomOrderAVL, resDelRandomOrderAVL, orderedData);
-        skipList_test(number, resInOrderedSL, resDelOrderedSL, orderedData);
-        skipList_test(number, resInReverseOrderSL,resDelReverseOrderSL, orderedData);
-        skipList_test(number, resInRandomOrderSL, resDelRandomOrderSL, orderedData);
+//        AVL_test(number, 0, orderedData);
+//        AVL_test(number, 1, reverseOrderedData);
+//        AVL_test(number, 2, randomOrderedData );
+        skipList_test(number, 0, orderedData);
+        skipList_test(number, 1, reverseOrderedData);
+        skipList_test(number, 2, randomOrderedData );
     }
 
     return 0;
 }
 
-void AVL_test(int number, std::ofstream& resultsIn, std::ofstream& resultsDel, std::ifstream& data){
+void AVL_test(int number, int type, std::ifstream& data){
+    std::ofstream resInOrderedAVL ("../tests/results/AVL/resultsPNOrderedIN.csv", std::ios::app);
+    std::ofstream resInReverseOrderAVL("../tests/results/AVL/resultsPNReverseOrderIN.csv", std::ios::app);
+    std::ofstream resInRandomOrderAVL ("../tests/results/AVL/resultsPNRandomOrderIN.csv", std::ios::app);
+    std::ofstream resDelOrderedAVL ("../tests/results/AVL/resultsPNOrderedDEL.csv", std::ios::app);
+    std::ofstream resDelReverseOrderAVL("../tests/results/AVL/resultsPNReverseOrderDEL.csv", std::ios::app);
+    std::ofstream resDelRandomOrderAVL ("../tests/results/AVL/resultsPNRandomOrderDEL.csv", std::ios::app);
+
     std::unique_ptr<AVLtree<int>> Tree = std::make_unique<AVLtree<int>>();
     int stream{};
     auto duration_start = std::chrono::high_resolution_clock::now();
@@ -57,7 +47,17 @@ void AVL_test(int number, std::ofstream& resultsIn, std::ofstream& resultsDel, s
     }
     auto duration_end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration<double, std::milli>(duration_end - duration_start).count();
-    resultsIn << "AVL, " << "insertion, " << number << ", " << duration << "\n";
+    switch (type){
+        case 0:
+            resInOrderedAVL  << "AVL, " << "insertion, " << number << ", " << duration << "\n";
+            break;
+        case 1:
+            resInReverseOrderAVL  << "AVL, " << "insertion, " << number << ", " << duration << "\n";
+            break;
+        case 2:
+            resInRandomOrderAVL << "AVL, " << "insertion, " << number << ", " << duration << "\n";
+            break;
+        }
 // jaka kolejność usuwania??
     duration_start = std::chrono::high_resolution_clock::now();
     for (int i{0}; i < number; ++i) {
@@ -66,10 +66,28 @@ void AVL_test(int number, std::ofstream& resultsIn, std::ofstream& resultsDel, s
     }
     duration_end = std::chrono::high_resolution_clock::now();
     duration = std::chrono::duration<double, std::milli>(duration_end - duration_start).count();
-    resultsDel << "AVL, " << "deletion, " << number << ", " << duration << "\n";
+    switch (type){
+        case 0:
+            resDelOrderedAVL  << "AVL, " << "deletion, " << number << ", " << duration << "\n";
+            break;
+        case 1:
+            resDelReverseOrderAVL  << "AVL, " << "deletion, " << number << ", " << duration << "\n";
+            break;
+        case 2:
+            resDelRandomOrderAVL << "AVL, " << "deletion, " << number << ", " << duration << "\n";
+            break;
+    }
 }
 
-void skipList_test(int number, std::ofstream& resultsIn, std::ofstream& resultsDel, std::ifstream& data){
+void skipList_test(int number, int type, std::ifstream& data){
+    std::ofstream resInOrderedSL ("../tests/results/SL/resultsPNOrderedIN.csv", std::ios::app);
+    std::ofstream resInReverseOrderSL("../tests/results/SL/resultsPNReverseOrderIN.csv", std::ios::app);
+    std::ofstream resInRandomOrderSL ("../tests/results/SL/resultsPNRandomOrderIN.csv", std::ios::app);
+
+    std::ofstream resDelOrderedSL ("../tests/results/SL/resultsPNOrderedDEL.csv", std::ios::app);
+    std::ofstream resDelReverseOrderSL("../tests/results/SL/resultsPNReverseOrderDEL.csv", std::ios::app);
+    std::ofstream resDelRandomOrderSL ("../tests/results/SL/resultsPNRandomOrderDEL.csv", std::ios::app);
+
     constexpr int chosenHeight{5};
     std::unique_ptr<SkipList<int>> List = std::make_unique<SkipList<int>>(chosenHeight);
     int stream{};
@@ -80,8 +98,21 @@ void skipList_test(int number, std::ofstream& resultsIn, std::ofstream& resultsD
     }
     auto duration_end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration<double, std::milli>(duration_end - duration_start).count();
-    resultsIn << "SL, " << "insertion, " << number << ", " << duration << "\n";
 
+    switch (type) {
+        case 0:
+            resInOrderedSL << "SL, "
+                           << "insertion, " << number << ", " << duration << "\n";
+            break;
+        case 1:
+            resInReverseOrderSL << "SL, "
+                                << "insertion, " << number << ", " << duration << "\n";
+            break;
+        case 2:
+            resInRandomOrderSL << "SL, "
+                               << "insertion, " << number << ", " << duration << "\n";
+            break;
+    }
     duration_start = std::chrono::high_resolution_clock::now();
     for (int i{0}; i < number; ++i) {
         data >> stream;
@@ -89,5 +120,16 @@ void skipList_test(int number, std::ofstream& resultsIn, std::ofstream& resultsD
     }
     duration_end = std::chrono::high_resolution_clock::now();
     duration = std::chrono::duration<double, std::milli>(duration_end - duration_start).count();
-    resultsDel << "SL, " << "deletion, " << number << ", " << duration << "\n";
+
+    switch (type){
+        case 0:
+            resDelOrderedSL  << "SL, " << "deletion, " << number << ", " << duration << "\n";
+            break;
+        case 1:
+            resDelReverseOrderSL  << "SL, " << "deletion, " << number << ", " << duration << "\n";
+            break;
+        case 2:
+            resDelRandomOrderSL << "SL, " << "deletion, " << number << ", " << duration << "\n";
+            break;
+    }
 }
